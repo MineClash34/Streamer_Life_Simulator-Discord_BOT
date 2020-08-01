7185e7c283ee770c9acbaa7e530768c098ad0f17
unknown <mineclashdu34@gmail.com>
2020-08-01 18:18:54 +0200

    Add events & commands folder

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
+const fs = require("fs")
+const Enmap = require("enmap")
 require('dotenv').config()
 
 client.login(process.env.TOKEN)
