f7872374345901b4e55ba3d626f515c7430e7040
unknown <mineclashdu34@gmail.com>
2020-08-01 19:52:42 +0200

    Bug patch

--- commandes/misc/ping.js
+++ commandes/misc/ping.js
@@ -1,16 +1,16 @@
 const Discord = require("discord.js")
-exports.run = async (message, Lang, args) => {
+exports.run = async (message, Lang, args, getRandomColor, client) => {
 
     let startTime = Date.now();
     let embed = new Discord.MessageEmbed()
-    .setColor(GetRandomColor())
-    .setDescription(`⏲ Bot's ping : ***${(new Date() - startTime).toFixed(0)} ms***\n💓 API's ping  : ***${Math.round(client.ping).toFixed(0)} ms***`)
+    .setColor(getRandomColor())
+    .setDescription(`⏲ Bot's ping : ***${(new Date() - startTime).toFixed(0)} ms***\n💓 API's ping  : ***${Math.round(client.ws.ping).toFixed(0)} ms***`)
     .setFooter(`Streamer Life Simulator Bot, By ${process.env.OWNER}`, "https://media.discordapp.net/attachments/714918035101581406/739162292020838460/logo.png")
     .setTimestamp();
     message.channel.send(embed).then((botmessage) => {
-        let embed = new Discord.RichEmbed()
-        .setColor(GetRandomColor())
-        .setDescription(`⏲ Bot's ping : ***${(new Date() - startTime - Math.round(client.ping)).toFixed(0)} ms***\n💓 APIS's ping : ***${Math.round(client.ping).toFixed(0)} ms***\n:satellite: Total ping : ***${(new Date() - startTime).toFixed(0)} ms***`)
+        let embed = new Discord.MessageEmbed()
+        .setColor(getRandomColor())
+        .setDescription(`⏲ Bot's ping : ***${(new Date() - startTime).toFixed(0)} ms***\n💓 APIS's ping : ***${Math.round(client.ws.ping).toFixed(0)} ms***\n:satellite: Total ping : ***${(new Date() - startTime).toFixed(0)} ms***`)
         .setFooter(`Streamer Life Simulator Bot, By ${process.env.OWNER}`, "https://media.discordapp.net/attachments/714918035101581406/739162292020838460/logo.png")
         .setTimestamp();
         botmessage.edit(embed);
