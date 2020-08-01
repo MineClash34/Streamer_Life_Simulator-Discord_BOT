dd568e03883d71a4ef2475021c79aa440b35e64b
unknown <mineclashdu34@gmail.com>
2020-08-01 21:27:48 +0200

    Bug patch

--- function/getRandomColor.js
+++ function/getRandomColor.js
@@ -2,7 +2,7 @@ module.exports = function () {
     var letters = '0123456789ABCDEF';
     var hexColor = '#';
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      hexColor += letters[Math.floor(Math.random() * 16)];
     };
     return hexColor;
   };
