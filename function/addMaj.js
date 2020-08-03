023b90f8bfbed4db0813132457c3a64d31a17e69
unknown <mineclashdu34@gmail.com>
2020-08-04 00:13:31 +0200

    Function bug patch

--- function/addMaj.js
+++ function/addMaj.js
@@ -1,5 +1,5 @@
-module.exports = function (text) {
-    let text = text.toString().toLowerCase().split("")
+module.exports = function (text1) {
+    let text = text1.toString().toLowerCase().split("")
     let firstLetter = text[0].toUpperCase()
     text.shift()
     text.unshift(firstLetter)
