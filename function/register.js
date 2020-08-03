d3af230227f9f743266be4872256ce6dfc296f31
unknown <mineclashdu34@gmail.com>
2020-08-03 16:43:10 +0200

    Sames syntaxe update

--- function/register.js
+++ function/register.js
@@ -3,8 +3,8 @@ module.exports = function(message) {
     message.reply("\n:flag_gb: Hello and Welcome in **SLS : Streamer Life Simulator**! To start, please select in which language you want to play, it can be changed later.\n\n:flag_fr: Bonjour et Bienvenue dans SLS : Streamer Life Simulator ! Pour commencer, veuillez m'indiquer dans quel langue vous souhaitez jouer ! Celui-ci peut être changer plus tard.").then(async (FirstMessage) => {
         await FirstMessage.react("🇬🇧");
         await FirstMessage.react("🇫🇷");
-        let filter = (reaction, userreact) => {
-            return ["🇫🇷", "🇬🇧"].includes(reaction.emoji.name) && userreact.id === message.author.id;
+        let filter = (reaction, userReact) => {
+            return ["🇫🇷", "🇬🇧"].includes(reaction.emoji.name) && userReact.id === message.author.id;
         };
         FirstMessage.awaitReactions(filter, {max: 1, time: 90000, errors: ['time']})
         .then(collected => {
